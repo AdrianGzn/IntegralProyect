a8e4f546b8e7057a46d1f4ea5d8942dbc416742d
Adrian <233318@ids.upchiapas.edu.mx>
2024-06-18 23:06:46 -0700

    Menu option cargada con datos

---
+++ src/data/menuOptions.jsx
@@ -0,0 +1,27 @@
+const options = {
+    managemente : [
+        {text: "Reportes"},
+        {text: "Recursos"}
+    ],
+    escolarControl: [
+        {text: "Boletas"},
+        {text: "Horarios"},
+        {text: "Reuniones"}
+    ],
+    resourses : [
+        {text: "Solicitudes"},
+        {text: "Aportes"}
+    ],
+    teacher: [
+        {text: "Asistencia"},
+        {text: "Ver horario"},
+        {text: "Reunión"},
+        {text: "Reportes"},
+        {text: "Alta de calificaciones"},
+        {text: "Ver boletas"}
+    ]
+}
+
+function getOptions () {
+    return options;
+}
