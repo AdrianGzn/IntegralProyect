fa5ab53904015ce1d7962d784d16fbabc88d072c
Regiber18 <regiber.montoya2005@hotmail.com>
2024-07-02 10:31:01 -0600

    segundo commit de teacher

---
+++ src/components/atoms/ButtonDownload.jsx
@@ -0,0 +1,10 @@
+
+
+function ButtonDownload({onClick}) {
+
+    return(
+        <button onClick={onClick}></button>
+    )
+}
+
+export default ButtonDownload;
--- src/pages/teacher/TeacherAsistance.jsx
+++ src/pages/teacher/TeacherAsistance.jsx
@@ -1,11 +1,14 @@
 import Header from "../../components/organisms/Header";
-
+import Input from "../../components/atoms/InputNavigate"
+import ButtonDownload from "../../components/atoms/ButtonDownload";
 function TeacherAsistance() {
     return (
         <div className="h-full w-full bg-slate-900">
             <Header role="teacher" />
-            <div className="w-full h-[80vh] flex justify-center items-center">
+            <div className="w-full h-[80vh] flex flex-wrap justify-center items-center">
+                <div className="min-h-[5%] w-4/6 border-2 border-white"><Input></Input></div>
                 <div className="h-4/5 w-4/6 border-2 border-white" />
+                <div className="min-h-[5%] w-4/6 border-2 border-white"><ButtonDownload></ButtonDownload></div>
             </div>
         </div>
     );
