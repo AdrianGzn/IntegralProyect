51955085cf22982c510f0294edaa0ffd03af5b9e
Regiber18 <regiber.montoya2005@hotmail.com>
2024-07-23 22:46:49 -0600

    commit cambios al pdf y post

--- src/components/organisms/ForminBallot.jsx
+++ src/components/organisms/ForminBallot.jsx
@@ -1,14 +1,14 @@
-import PDFformat from "../../components/atoms/PDFformat";
-import { BlobProvider } from '@react-pdf/renderer';
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import Swal from "sweetalert2";
 import Field from "../molecules/Field";
 import Button from "../atoms/Button";
 import Text from "../atoms/Text";
+import { BlobProvider } from '@react-pdf/renderer';
+import PDFformat from "../../components/atoms/PDFformat";
 
 function ForminBallot() {
     const [content, setContent] = useState({});
-    const [PDFBase64, setPDFBase64] = useState('');
+    const [pdfBlob, setPdfBlob] = useState(null);
     const ratingFinal = useRef(null);
     const observations = useRef(null);
     const subject1 = useRef(null);
@@ -20,142 +20,92 @@ function ForminBallot() {
     const subject4 = useRef(null);
     const rating4 = useRef(null);
 
-    const data = [
-        {
-            ratingFinal
-        }
-    ]
-
-    
-    const convertBlobToBase64 = (blob) => {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onloadend = () => resolve(reader.result);
-            reader.onerror = reject;
-            reader.readAsDataURL(blob);
-        });
-    };
-
-    useEffect(() => {
-        if (PDFBase64) {
-            return () => URL.revokeObjectURL(PDFBase64);
-        }
-    }, [PDFBase64]);
 
     const addBallot = () => {
-        const newContent = {
-            observations: observations.current.value,
-            ratingFinal: ratingFinal.current.value,
-            subject1: subject1.current.value,
-            rating1: rating1.current.value,
-            subject2: subject2.current.value,
-            rating2: rating2.current.value,
-            subject3: subject3.current.value,
-            rating3: rating3.current.value,
-            subject4: subject4.current.value,
-            rating4: rating4.current.value,
-        };
+        if (!pdfBlob) {
+            Swal.fire({
+                title: "Error",
+                text: "Debe generar primero el PDF",
+                icon: "error"
+            });
+            return;
+        }
+        const blobUrl = URL.createObjectURL(pdfBlob); // Obtener la URL del Blob
 
-        setContent(newContent);
-
-        Swal.fire({
-            title: "Agregar boleta",
-            text: "¿Desea agregar la boleta?",
-            icon: "question",
-            showCancelButton: true,
-            confirmButtonText: "Agregar",
-            cancelButtonText: "Cancelar",
-        }).then(response => {
-            if (response.isConfirmed) {
-                convertBlobToBase64(PDFBase64).then(base64 => {
-                    fetch(`${import.meta.env.VITE_URL}/ballot`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Control-Acces-Allow-Origin': '*'
-                        },
-                        body: JSON.stringify({
-                            alumn_id: 1,
-                            content: observations,
-                            created_by: "teacher",
-                            updated_by: "teacher",
-                            deleted: false
-                        })
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log('Success:', data);
-                        Swal.fire({
-                            title: "Agregado",
-                            text: "Se agregó la boleta",
-                            icon: "success"
-                        });
-                    })
-                    .catch((error) => {
-                        console.error('Error:', error);
-                        Swal.fire({
-                            title: "No se logró agregar",
-                            text: "No se pudo agregar la boleta",
-                            icon: "error"
-                        });
-                    });
-                }).catch(error => {
-                    console.error('Error converting Blob to Base64:', error);
-                    Swal.fire({
-                        title: "Error",
-                        text: "No se pudo convertir el PDF a Base64",
-                        icon: "error"
-                    });
-                });
+        fetch(`${import.meta.env.VITE_URL}/ballot`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                alumn_id: 1,
+                name: 'regi',
+                url: blobUrl, // Enviar la URL como path en el POST
+                created_by: 'teacher',
+                updated_by: 'teacher',
+                deleted: false
+            })
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
             } else {
-                Swal.fire({
-                    title: "No se logró agregar",
-                    text: "No se pudo agregar la boleta",
-                    icon: "error"
-                });
+                throw new Error('Network response was not ok');
             }
+        })
+        .then(data => {
+            console.log('Success:', data);
+            Swal.fire({
+                title: "Agregado",
+                text: "Se agregó la boleta",
+                icon: "success"
+            });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            Swal.fire({
+                title: "No se logró agregar",
+                text: "No se pudo agregar la boleta",
+                icon: "error"
+            });
         });
     };
 
     return (
         <>
-                <div className="w-full flex items-center justify-evenly flex-wrap">
-                    
-                    <div className="w-1/4 min-w-64 mb-10 flex justify-center flex-wrap">
-                        <Text text="Materias" className="!text-xl underline decoration-lime-500"></Text>
-                        <Field text="Materia 1:" type="text" placeholder="materia" inputRef={subject1} className="w-[90%]" />
-                        <Field text="Materia 2:" type="text" placeholder="materia" inputRef={subject2} className="w-[90%]" />
-                        <Field text="Materia 3:" type="text" placeholder="materia" inputRef={subject3} className="w-[90%]" />
-                        <Field text="Materia 4:" type="text" placeholder="materia" inputRef={subject4} className="w-[90%]" />
-                    </div>
-                    <div className="w-1/4 min-w-64 mb-10 flex justify-center flex-wrap">
-                        <Text text="Calificaciones" className="!text-xl underline decoration-lime-500"></Text>
-                        <Field text="Calificación 1:" type="text" placeholder="calificación" inputRef={rating1} className="w-[90%]" />
-                        <Field text="Calificación 2:" type="text" placeholder="calificación" inputRef={rating2} className="w-[90%]" />
-                        <Field text="Calificación 3:" type="text" placeholder="calificación" inputRef={rating3} className="w-[90%]" />
-                        <Field text="Calificación 4:" type="text" placeholder="calificación" inputRef={rating4} className="w-[90%]" />
-                    </div>
-                    <div className="w-[30%] min-w-64 mb-10 flex justify-center flex-wrap">
-                        <Text text="General" className="!text-xl underline decoration-lime-500"></Text>
-                        <Field text="Observaciones:" type="text" placeholder="observaciones" inputRef={observations} className="w-[90%]" />
-                        <Field text="Calificación final:" type="text" placeholder="calificación" inputRef={ratingFinal} className="w-[90%]" />
-                    </div>
+            <div className="w-full flex items-center justify-evenly flex-wrap">
+                <div className="w-1/4 min-w-64 mb-10 flex justify-center flex-wrap">
+                    <Text text="Materias" className="!text-xl underline decoration-lime-500"></Text>
+                    <Field text="Materia 1:" type="text" placeholder="materia" inputRef={subject1} className="w-[90%]" />
+                    <Field text="Materia 2:" type="text" placeholder="materia" inputRef={subject2} className="w-[90%]" />
+                    <Field text="Materia 3:" type="text" placeholder="materia" inputRef={subject3} className="w-[90%]" />
+                    <Field text="Materia 4:" type="text" placeholder="materia" inputRef={subject4} className="w-[90%]" />
                 </div>
-                <div className="flex flex-col items-center justify-center mt-4 w-full h-[50]">
-                    <Button className="mb-4" text="Guardar" onClick={addBallot} />
-                    <BlobProvider document={<PDFformat ratingFinal={content.ratingFinal || ''} observations={content.observations} subject1={content.subject1 || ''} subject2={content.subject2} subject3={content.subject3} subject4={content.subject4} />}>
-                        {({ blob, url }) => {
-                            if (blob && url) {
-                                setPDFBase64(blob);
-                                return (
-                                    <a href={url} download="ballot.pdf" className="btn btn-primary">Descargar PDF</a>
-                                );
-                            } else {
-                                return <span>No se pudo generar el PDF</span>;
-                            }
-                        }}
-                    </BlobProvider>
+                <div className="w-1/4 min-w-64 mb-10 flex justify-center flex-wrap">
+                    <Text text="Calificaciones" className="!text-xl underline decoration-lime-500"></Text>
+                    <Field text="Calificación 1:" type="text" placeholder="calificación" inputRef={rating1} className="w-[90%]" />
+                    <Field text="Calificación 2:" type="text" placeholder="calificación" inputRef={rating2} className="w-[90%]" />
+                    <Field text="Calificación 3:" type="text" placeholder="calificación" inputRef={rating3} className="w-[90%]" />
+                    <Field text="Calificación 4:" type="text" placeholder="calificación" inputRef={rating4} className="w-[90%]" />
                 </div>
+                <div className="w-[30%] min-w-64 mb-10 flex justify-center flex-wrap">
+                    <Text text="General" className="!text-xl underline decoration-lime-500"></Text>
+                    <Field text="Observaciones:" type="text" placeholder="observaciones" inputRef={observations} className="w-[90%]" />
+                    <Field text="Calificación final:" type="text" placeholder="calificación" inputRef={ratingFinal} className="w-[90%]" />
+                </div>
+            </div>
+            <div className="flex flex-col items-center justify-center mt-4 w-full h-[50]">
+                <Button className="mb-4" text="Guardar" onClick={addBallot} />
+                <BlobProvider document={<PDFformat ratingFinal={content.ratingFinal || ''} observations={content.observations} subject1={content.subject1 || ''} subject2={content.subject2} subject3={content.subject3} subject4={content.subject4} />}>
+                    {({ blob }) => {
+                        if (blob) {
+                            setPdfBlob(blob);
+                        } else {
+                            return <span>No se pudo generar el PDF</span>;
+                        }
+                    }}
+                </BlobProvider>
+            </div>
         </>
     );
 }
--- src/pages/teacher/TeacherBallots.jsx
+++ src/pages/teacher/TeacherBallots.jsx
@@ -1,45 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/organisms/Header';
 import SearchBallotSection from '../../components/organisms/SearchBallotSection';
 import Swal from 'sweetalert2';
 import '@sweetalert2/theme-bulma';
-import PDFButton from '../../components/atoms/PDFButton';
-/*import { pdf } from '@react-pdf/renderer';
-var AWS = require('aws-sdk')*/
+import PDFViewerComponent from '../../components/atoms/PDFViewerComponent';
+import "@pdfslick/react/dist/pdf_viewer.css";
+import { data } from 'autoprefixer';
+
+
 
 function TeacherBallots() {
     const [matricleSearch, setMatricleSearch] = useState('');
     const [pdfUrls, setPdfUrls] = useState([]);
     const [newPDFs, setNewPDFS] = useState([]);
 
-    const uploadFile = async (file) => {
-        const S3_BUCKET = "pdf-school";
-        const REGION = "us-east-1";
-
-        AWS.config.update({
-            accessKeyId: "youraccesskeyhere",
-            secretAccessKey: "yoursecretaccesskeyhere",
-        });
-
-        const s3 = new AWS.S3({
-            params: { Bucket: S3_BUCKET },
-            region: REGION,
-        });
-
-        const params = {
-            Bucket: S3_BUCKET,
-            Key: file.name,
-            Body: file,
-        };
-
-        try {
-            await s3.putObject(params).promise();
-            alert("File uploaded successfully.");
-        } catch (error) {
-            console.error("Error uploading file: ", error);
-            alert("Error uploading file. Please try again.");
-        }
-    };
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_URL}/ballot`)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then((data) => {
+                setPdfUrls(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching ballot data:', error);
+            });
+    }, []);
 
     const filteredPDF = (e) => {
         const searchTerm = e.target.value.toLowerCase();
@@ -56,24 +45,17 @@ function TeacherBallots() {
                 <div className="h-4/5 w-4/6 lg:w-4/6 flex flex-col wrap items-center border-2 border-white">
                     <SearchBallotSection val={matricleSearch} fnVal={setMatricleSearch} onChange={filteredPDF} />
                     <div className='h-[60%] w-full overflow-x-hidden flex align-items-center gap-10 p-10'>
-                        {
-                            newPDFs.length === 0 ?
-                                (
-                                    <p>No se encontraron boletas</p>
-                                ) : (
-                                    newPDFs.map((pdfUrl, index) => (
-                                        <div key={index}>
-                                            <a href={pdfUrl} download={`boleta-${index + 1}.pdf`}><img src="../../public/icon_pdf.png" alt="Icon" className="w-5 h-5 mr-2" /></a>
-                                            <h6>Boleta {index + 1}</h6>
-                                        </div>
-                                    ))
-                                )
-                        }
+                        {newPDFs.map((pdfUrl, index) => (
+                            <>
+                                <p>{pdfUrl.name}</p>
+                            </>
+                        )
+                        )}
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default TeacherBallots;
