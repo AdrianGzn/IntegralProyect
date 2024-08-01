c768380a37db95a3c169d04ae1fdd577ea8c5caa
Regiber18 <regiber.montoya2005@hotmail.com>
2024-07-31 22:01:46 -0600

    cambios en teacher

--- src/pages/teacher/TeacherQualifications.jsx
+++ src/pages/teacher/TeacherQualifications.jsx
@@ -1,15 +1,24 @@
-import React from "react";
-import Header from "../../components/organisms/Header";
+import React, { useState } from 'react';
+import Header from '../../components/organisms/Header';
+import Table from '../../components/organisms/Table';
 
 function TeacherQualifications() {
-    return <div className="min-h-screen w-full bg-slate-900">
-        <Header role="teacher" />
-        <div className="w-full flex justify-center items-center">
-            <div className="h-[75vh] w-4/6 flex flex-col">
-                Sección de calificaciones
+    const [data, setData] = useState([
+        { col1: 'Data 1', col2: 'Data 2', col3: 'Data 3', col4: 'Data 4', col5: 'Data 5', col6: 'Data 6', col7: 'Data 7' },
+        { col1: 'Data 8', col2: 'Data 9', col3: 'Data 10', col4: 'Data 11', col5: 'Data 12', col6: 'Data 13', col7: 'Data 14' },
+        { col1: 'Data 15', col2: 'Data 16', col3: 'Data 17', col4: 'Data 18', col5: 'Data 19', col6: 'Data 20', col7: 'Data 21' }
+    ]);
+
+    return (
+        <div className="min-h-screen w-full bg-slate-900">
+            <Header role="teacher" />
+            <div className="w-full flex justify-center items-center">
+                <div className="h-[75vh] w-4/6 flex flex-col p-4">
+                    <Table title="Teacher Qualifications" data={data} />
+                </div>
             </div>
         </div>
-    </div>
+    );
 }
 
 export default TeacherQualifications;
