2c8e1200c5b326f201d23cbf74ed8c331d7e399f
Adrian <233318@ids.upchiapas.edu.mx>
2024-08-04 21:27:41 -0700

    Obtención de datos para put de ratings

--- src/components/organisms/Table.jsx
+++ src/components/organisms/Table.jsx
@@ -4,117 +4,117 @@ import html2canvas from 'html2canvas';
 import { format } from 'date-fns';
 import Th from '../atoms/Th';
 
-function Table({ data, size, headers }) {
-  const [rows, setRows] = useState(data);
-  const [editing, setEditing] = useState({ rowIndex: null, colIndex: null });
+function Table({ data, size, headers, onBlur }) {
+    const [rows, setRows] = useState(data);
+    const [editing, setEditing] = useState({ rowIndex: null, colIndex: null });
 
-  useEffect(() => {
-    setRows(data);
-  }, [data]);
+    useEffect(() => {
+        setRows(data);
+    }, [data]);
 
-  const handleCellClick = (rowIndex, colIndex) => {
-    // Permitir edición solo en las columnas 4, 5, 6
-    if ([3, 4, 5].includes(colIndex)) {
-      setEditing({ rowIndex, colIndex });
-    }
-  };
-
-  const handleCellChange = (e, rowIndex, colIndex) => {
-    const newRows = [...rows];
-    newRows[rowIndex][`col${colIndex + 1}`] = e.target.value;
-    setRows(newRows);
-  };
-
-  const handleCellBlur = () => {
-    setEditing({ rowIndex: null, colIndex: null });
-  };
-
-  const generatePDF = () => {
-    const input = document.getElementById('table-container');
-    const currentDate = new Date();
-    const formattedDate = format(currentDate, 'dd/MM/yyyy HH:mm:ss');
-    
-    html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF();
-        const imgWidth = 210; 
-        const pageHeight = 500;
-        const imgHeight = (canvas.height * imgWidth) / canvas.width;
-        let heightLeft = imgHeight;
-        let position = 0;
-
-        pdf.text(`Fecha: ${formattedDate}`, 10, 10);
-        position = 20; // Deja un poco de espacio para la fecha
-        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-
-        while (heightLeft >= 0) {
-          position = heightLeft - imgHeight;
-          pdf.addPage();
-          pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-          heightLeft -= pageHeight;
+    const handleCellClick = (rowIndex, colIndex) => {
+        if ([3, 4, 5].includes(colIndex)) {
+            setEditing({ rowIndex, colIndex });
         }
-        const pdfOutput = pdf.output('blob');
-        const url = URL.createObjectURL(pdfOutput);
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = 'table.pdf';
-        link.click();
-        URL.revokeObjectURL(url);
-      })
-      .catch(error => {
-        console.error('Error generando el PDF:', error);
-      });
-  };
+    };
 
-  return (
-    <div>
-      <button
-        onClick={generatePDF}
-        className="bg-blue-600 text-white p-3 rounded mb-4 hover:bg-blue-700 transition-colors font-bold shadow-lg"
-      >
-        Generar PDF
-      </button>
-      <div id="table-container">
-        <table className="min-w-full divide-y divide-gray-300 bg-white shadow-md rounded-md border border-gray-300">
-          <thead className="bg-gray-600 text-white">
-            <tr>
-              {headers.map((header, index) => (
-                <Th key={index} text={header} />
-              ))}
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-300">
-            {rows.map((row, rowIndex) => (
-              <tr key={rowIndex} className="hover:bg-gray-100 transition-colors">
-                {[...Array(size).keys()].map((_, colIndex) => (
-                  <td
-                    key={colIndex}
-                    className={`px-4 py-4 whitespace-nowrap border-r border-gray-300 ${editing.rowIndex === rowIndex && editing.colIndex === colIndex ? 'bg-gray-100' : ''}`}
-                    onClick={() => handleCellClick(rowIndex, colIndex)}
-                  >
-                    {editing.rowIndex === rowIndex && editing.colIndex === colIndex ? (
-                      <input
-                        type="text"
-                        value={row[`col${colIndex + 1}`] || ''}
-                        onChange={(e) => handleCellChange(e, rowIndex, colIndex)}
-                        onBlur={handleCellBlur}
-                        autoFocus
-                        className="w-[60%] border border-gray-300 rounded px-2 py-1"
-                      />
-                    ) : (
-                      row[`col${colIndex + 1}`] || ''
-                    )}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    const handleCellChange = (e, rowIndex, colIndex) => {
+        const newRows = [...rows];
+        newRows[rowIndex][`col${colIndex + 1}`] = e.target.value;
+        setRows(newRows);
+    };
+
+    const handleCellBlur = (rowIndex, colIndex) => {
+        const newValue = rows[rowIndex][`col${colIndex + 1}`];
+        setEditing({ rowIndex: null, colIndex: null });
+        onBlur(rowIndex, colIndex, newValue);
+    };
+
+    const generatePDF = () => {
+        const input = document.getElementById('table-container');
+        const currentDate = new Date();
+        const formattedDate = format(currentDate, 'dd/MM/yyyy HH:mm:ss');
+
+        html2canvas(input)
+            .then((canvas) => {
+                const imgData = canvas.toDataURL('image/png');
+                const pdf = new jsPDF();
+                const imgWidth = 210;
+                const pageHeight = 500;
+                const imgHeight = (canvas.height * imgWidth) / canvas.width;
+                let heightLeft = imgHeight;
+                let position = 0;
+
+                pdf.text(`Fecha: ${formattedDate}`, 10, 10);
+                position = 20; // Deja un poco de espacio para la fecha
+                pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+                heightLeft -= pageHeight;
+
+                while (heightLeft >= 0) {
+                    position = heightLeft - imgHeight;
+                    pdf.addPage();
+                    pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+                    heightLeft -= pageHeight;
+                }
+                const pdfOutput = pdf.output('blob');
+                const url = URL.createObjectURL(pdfOutput);
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = 'table.pdf';
+                link.click();
+            })
+            .catch((error) => {
+                console.error('Error generando el PDF:', error);
+            });
+    };
+
+    return (
+        <div>
+            <button
+                onClick={generatePDF}
+                className="bg-blue-600 text-white p-3 rounded mb-4 hover:bg-blue-700 transition-colors font-bold shadow-lg"
+            >
+                Generar PDF
+            </button>
+            <div id="table-container">
+                <table className="min-w-full divide-y divide-gray-300 bg-white shadow-md rounded-md border border-gray-300">
+                    <thead className="bg-gray-600 text-white">
+                        <tr>
+                            {headers.map((header, index) => (
+                                <Th key={index} text={header} />
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-300">
+                        {rows.map((row, rowIndex) => (
+                            <tr key={rowIndex} className="hover:bg-gray-100 transition-colors">
+                                {[...Array(size).keys()].map((_, colIndex) => (
+                                    <td
+                                        key={colIndex}
+                                        className={`px-4 py-4 whitespace-nowrap border-r border-gray-300 ${editing.rowIndex === rowIndex && editing.colIndex === colIndex ? 'bg-gray-100' : ''}`}
+                                        onClick={() => handleCellClick(rowIndex, colIndex)}
+                                    >
+                                        {editing.rowIndex === rowIndex && editing.colIndex === colIndex ? (
+                                            <input
+                                                type="text"
+                                                value={row[`col${colIndex + 1}`] || ''}
+                                                onChange={(e) => handleCellChange(e, rowIndex, colIndex)}
+                                                onBlur={() => handleCellBlur(rowIndex, colIndex)}
+                                                autoFocus
+                                                className="w-[60%] border border-gray-300 rounded px-2 py-1"
+                                            />
+                                        ) : (
+                                            row[`col${colIndex + 1}`] || ''
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default Table;
--- src/pages/teacher/TeacherQualifications.jsx
+++ src/pages/teacher/TeacherQualifications.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../../components/organisms/Header';
 import Table from '../../components/organisms/Table';
+import Swal from 'sweetalert2';
 import { getId } from '../../data/userActual';
+import { update } from 'lodash';
 
 function TeacherQualifications() {
+    const [auxData, setAuxData] = useState([]);
     const [data, setData] = useState([]);
     const [alumns, setAlumns] = useState([]);
     const headers = ["Num lista", "Nombre", "Apellidos", "Español", "Matemáticas", "Ciencias", "Calificación final"];
@@ -17,24 +20,24 @@ function TeacherQualifications() {
                 'Access-Control-Allow-Origin': '*',
             },
         })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('La respuesta no es ok.');
-            }
-        })
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].personal_id == getId()) {
-                    setAlumns(data[i].alumns || []);
-                    break;
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('La respuesta no es ok.');
                 }
-            }
-        })
-        .catch(error => {
-            console.log("Ha ocurrido un error: " + error);
-        });
+            })
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].personal_id == getId()) {
+                        setAlumns(data[i].alumns || []);
+                        break;
+                    }
+                }
+            })
+            .catch(error => {
+                console.log("Ha ocurrido un error: " + error);
+            });
     }, []);
 
     useEffect(() => { // Calificación final
@@ -45,81 +48,95 @@ function TeacherQualifications() {
                     'Content-Type': 'application/json',
                 },
             })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('La respuesta no es ok.');
-                }
-            })
-            .then(subjectData => {
-                const alumnIds = new Set(alumns.map(item => item.alumn_id));
-                const filteredData = subjectData.filter(subject => alumnIds.has(subject.alumn_id));
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('La respuesta no es ok.');
+                    }
+                })
+                .then(subjectData => {
+                    const alumnIds = new Set(alumns.map(item => item.alumn_id));
+                    const filteredData = subjectData.filter(subject => alumnIds.has(subject.alumn_id));
 
-                const newData = filteredData.map(subject => {
-                    const alumn = alumns.find(a => a.alumn_id == subject.alumn_id);
-                    return {
-                        col1: alumn.alumn_id,
-                        col2: alumn.name,
-                        col3: alumn.lastName,
-                        col4: subject.espanol || 0,
-                        col5: subject.matematicas || 0,
-                        col6: subject.ciencias || 0,
-                        col7: subject.averageAmount || 0
-                    };
+                    const newData = filteredData.map(subject => {
+                        const alumn = alumns.find(a => a.alumn_id == subject.alumn_id);
+                        return {
+                            col1: alumn.alumn_id,
+                            col2: alumn.name,
+                            col3: alumn.lastName,
+                            col4: subject.espanol || 0,
+                            col5: subject.matematicas || 0,
+                            col6: subject.ciencias || 0,
+                            col7: subject.averageAmount || 0
+                        };
+                    });
+
+                    setData(newData);
+                    setAuxData(newData);
+                    setIterations(iterations + 1);
+                })
+                .catch(error => {
+                    console.log("Ha ocurrido un error: " + error);
                 });
-
-                setData(newData);
-                setIterations(iterations + 1);
-            })
-            .catch(error => {
-                console.log("Ha ocurrido un error: " + error);
-            });
         }
     }, [alumns]);
 
-    const onBlur = () => {
-        fetch(`${import.meta.env.VITE_URL}/alumn`, {
+    const onBlur = (rowIndex, colIndex, newValue) => {
+        console.log(rowIndex);
+        console.log(colIndex + 1);
+        console.log(newValue);
+
+        if(colIndex == 4){
+            subject = "Spanish";
+        }else if(colIndex == 5){
+            subject = "Math";
+        }else if(colIndex == 6){
+            subject = "Science";
+        }
+
+        const newAlumnId = alumns[rowIndex].alumn_id;
+        
+        fetch(`${import.meta.env.VITE_URL}/ratings`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                class_id:1,
-                name: nameRef.current.value,
-                lastName: fathersLastNameRef.current.value,
-                lastName: mothersLastNameRef.current.value,
-                created_by: 'escolarControl',
-                updated_by: 'escolarControl',
-                deleted: false
+                alumn_id: newAlumnId,
+                amount: newValue,
+                pertenence: subject,
+                gradePertenence: 1,
+                created_by: "Teacher",
+                updated_by: "Teacher"
             })
         })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Network response was not ok');
-            }
-        })
-        .then(data => {
-            console.log('Success:', data);
-            Swal.fire({
-                title: "Agregado",
-                text: "Se agregó el alumno",
-                icon: "success"
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Network response was not ok');
+                }
+            })
+            .then(data => {
+                console.log('Success:', data);
+                Swal.fire({
+                    title: "Agregado",
+                    text: "Se agregó la calificación",
+                    icon: "success"
+                });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                Swal.fire({
+                    title: "No se logró agregar",
+                    text: "No se pudo agregar la calificación",
+                    icon: "error"
+                });
             });
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            Swal.fire({
-                title: "No se logró agregar",
-                text: "No se pudo agregar el alumno",
-                icon: "error"
-            });
-        });
     }
 
-    useEffect(() => { // Este es el useEffect que me encontraba trabajando
+    useEffect(() => {
         if (iterations > 0) {
             fetch(`${import.meta.env.VITE_URL}/rating`, {
                 method: 'GET',
@@ -127,38 +144,38 @@ function TeacherQualifications() {
                     'Content-Type': 'application/json',
                 },
             })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('La respuesta no es ok.');
-                }
-            })
-            .then(ratings => {
-                const updatedData = data.map(item => {
-                    const matchedRatings = ratings.filter(rating => rating.alumn_id === item.col1);
-                    
-                    let updatedItem = { ...item };
-
-                    for (let i = 0; i < matchedRatings.length; i++) {
-                        const rating = matchedRatings[i];
-                        if (rating.pertenence === "Spanish") {
-                            updatedItem.col4 = rating.amount;
-                        } else if (rating.pertenence === "Math") {
-                            updatedItem.col5 = rating.amount;
-                        } else if (rating.pertenence === "Science") {
-                            updatedItem.col6 = rating.amount;
-                        }
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('La respuesta no es ok.');
                     }
+                })
+                .then(ratings => {
+                    const updatedData = data.map(item => {
+                        const matchedRatings = ratings.filter(rating => rating.alumn_id === item.col1);
 
-                    return updatedItem;
+                        let updatedItem = { ...item };
+
+                        for (let i = 0; i < matchedRatings.length; i++) {
+                            const rating = matchedRatings[i];
+                            if (rating.pertenence === "Spanish") {
+                                updatedItem.col4 = rating.amount;
+                            } else if (rating.pertenence === "Math") {
+                                updatedItem.col5 = rating.amount;
+                            } else if (rating.pertenence === "Science") {
+                                updatedItem.col6 = rating.amount;
+                            }
+                        }
+
+                        return updatedItem;
+                    });
+
+                    setData(updatedData);
+                })
+                .catch(error => {
+                    console.log("Ha ocurrido un error: " + error);
                 });
-
-                setData(updatedData);
-            })
-            .catch(error => {
-                console.log("Ha ocurrido un error: " + error);
-            });
         }
     }, [iterations]);
 
@@ -167,11 +184,12 @@ function TeacherQualifications() {
             <Header role="teacher" />
             <div className="w-full flex justify-center items-center">
                 <div className="h-[75vh] w-4/6 flex flex-col p-4">
-                    <Table 
-                        title="Teacher Qualifications" 
+                    <Table
+                        title="Teacher Qualifications"
                         headers={headers}
                         data={data}
                         size={7}
+                        onBlur={onBlur}
                     />
                 </div>
             </div>
